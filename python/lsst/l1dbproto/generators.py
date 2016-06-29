9b2787719cbfb4467e9665333de56813e0e97a68
Andy Salnikov <salnikov@slac.stanford.edu>
2016-06-29 16:09:34 -0700

    Add generator for cones

--- python/lsst/l1dbproto/generators.py
+++ python/lsst/l1dbproto/generators.py
@@ -5,6 +5,7 @@ Module defining methods for generating random things.
 #--------------------------------
 #  Imports of standard modules --
 #--------------------------------
+import math
 import numpy
 
 #-----------------------------
@@ -14,6 +15,24 @@ import numpy
 #----------------------------------
 # Local non-exported definitions --
 #----------------------------------
+
+def _rotation_matrix(a, b):
+    """
+    Create rotation matrix to rotate vector a into b.
+
+    After http://math.stackexchange.com/a/476311
+    """
+
+    v = numpy.cross(a, b)
+    sin = numpy.linalg.norm(v)
+    if sin == 0:
+        return numpy.identity(3)
+    cos = numpy.vdot(a, b)
+    vx = numpy.mat([[0, -v[2], v[1]], [v[2], 0., -v[0]], [-v[1], v[0], 0.]])
+
+    R = numpy.identity(3) + vx + vx * vx * (1 - cos) / (sin ** 2)
+
+    return R
 
 #------------------------
 # Exported definitions --
@@ -44,3 +63,28 @@ def rand_sphere_xyz(count=1, hemi=0, seed=None):
 
     return r
 
+
+def rand_cone_xyz(direction, open_angle, n=1, seed=None):
+    """
+    Generate random vectors in a cone around given vector.
+
+    @param direction: XYZ vector of the cone axis, must be numpy array
+    @param open_angle: opening angle (full) of a cone, radians
+    @param n:         number of generated vectors
+    @param seed:      seed for generator
+    """
+    rs = numpy.random.RandomState(seed=seed)
+    xy = rs.normal(size=(n, 2))
+    xy /= numpy.linalg.norm(xy, axis=1)[:, numpy.newaxis]
+
+    z0 = math.cos(open_angle / 2)
+    zs = rs.uniform(z0, 1., size=n)
+    rs = numpy.sqrt(1.0 - zs * zs)
+
+    res = numpy.empty((n, 3), 'f8')
+    res[:, :2] = rs[:, numpy.newaxis] * xy
+    res[:, 2] = zs
+
+    # rotate
+    R = _rotation_matrix(numpy.array([0., 0., 1.]), direction)
+    return numpy.inner(res, R)
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -7,6 +7,7 @@ Unit tests for l1dbproto.generators module.
 #--------------------------------
 #  Imports of standard modules --
 #--------------------------------
+import math
 import numpy
 import unittest
 
@@ -31,7 +32,8 @@ class test_generators(unittest.TestCase):
     def tearDown(self) :
         pass
 
-    def test_xyz(self):
+    def test_sphere_xyz(self):
+        """ Test for points-on-sphere generator """
 
         points = generators.rand_sphere_xyz(1)
         self.assertEqual(points.shape, (1, 3))
@@ -51,6 +53,47 @@ class test_generators(unittest.TestCase):
         # covers southern hemisphere, z in range -1 to 0
         points = generators.rand_sphere_xyz(100, hemi=-1)
         self.assertLess(max(points[:, 2]), 0)
+
+
+    def test_rot_matrix(self):
+        """ Testing rotation matrix method """
+
+        a = numpy.array([0., 0., 1.])
+        b = numpy.array([0., 0., 1.])
+        R = generators._rotation_matrix(a, b)
+        self.assert_(numpy.array_equal(R, numpy.identity(3)))
+
+        a = numpy.array([1., 0., 0.])
+        b = numpy.array([0., 0., 1.])
+        R = generators._rotation_matrix(a, b)
+        expected = numpy.matrix("0,0,-1;0,1,0;1,0,0", dtype=float)
+        self.assert_(numpy.array_equal(R, expected))
+
+        a = numpy.array([0., -1., 0.])
+        b = numpy.array([0., 0., 1.])
+        R = generators._rotation_matrix(a, b)
+        expected = numpy.matrix("1,0,0;0,0,1;0,-1,0", dtype=float)
+        self.assert_(numpy.array_equal(R, expected))
+
+
+    def test_cone_xyz(self):
+        """ Test for vector-in-cone generator """
+
+        points = generators.rand_cone_xyz(numpy.array([0., 0., 1.]), 0., 1)
+        self.assertEqual(points.shape, (1, 3))
+
+        points = generators.rand_cone_xyz(numpy.array([0., 0., 1.]), 0., 100)
+        for norm in numpy.linalg.norm(points, axis=1):
+            self.assertAlmostEqual(norm, 1.)
+        for point in points:
+            self.assert_(numpy.array_equal(point, numpy.array([0., 0., 1.])))
+
+        points = generators.rand_cone_xyz(numpy.array([0., 0., 1.]), math.pi / 2, 100)
+        self.assertGreater(min(points[:, 2]), math.cos(math.pi / 4))
+        self.assertGreater(min(points[:, 0]), -math.cos(math.pi / 4))
+        self.assertLess(max(points[:, 0]), math.cos(math.pi / 4))
+        self.assertGreater(min(points[:, 1]), -math.cos(math.pi / 4))
+        self.assertLess(max(points[:, 1]), math.cos(math.pi / 4))
 
 
 #
